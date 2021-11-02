4117fbc63b05c795b1c0e93c8fef7a7fce1ec420
zimzoom <7947215+zimzoom@users.noreply.github.com>
2021-11-02 17:13:05 -0500

    Selenium scrolls all the way down and saves

--- selen3.py
+++ selen3.py
@@ -18,8 +18,29 @@ driver.get(url)
 time.sleep(3)
 #print(driver.find_element_by_id('content').text)
 
+SCROLL_PAUSE_TIME = 0.5
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+# Save source to file
 pageSource = driver.page_source
 with open(filename, 'w') as file:
 	file.write(pageSource)
 
-driver.close()
+# Close browser
+input("Press enter to end session")
+driver.quit()
